8d137f6b6ceee2a94c3795dd133b3ade4343f4cb
Liukeyu <liukele015@gmail.com>
2025-10-08 18:47:05 +0800

    添加 DebuffContainer 类及相关命名空间引用
    在 `DebuffContainer.cs` 文件中新增了多个命名空间引用。添加了 `DebuffContainer` 类，继承自 `KMonoBehaviour`，并在 `OnSpawn` 方法中确保只添加一次 `AbyssophobiaDebuff` 组件。引入了 `IsEnabled` 布尔变量以控制容器的启用状态。

---
+++ EternalDecay/Content/Comps/DebuffCom/DebuffContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EternalDecay.Content.Comps.DebuffCom
+{
+    // 这是一个空组件，用作容器
+    public class DebuffContainer : KMonoBehaviour
+    {
+        // 可选：控制整个容器是否启用
+        public bool IsEnabled = true;
+
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            // 确保只添加一次 AbyssophobiaDebuff
+            if (this.gameObject.GetComponent<AbyssophobiaDebuff>() == null)
+                this.gameObject.AddComponent<AbyssophobiaDebuff>();
+
+
+
+        }
+
+
+    }
+}
