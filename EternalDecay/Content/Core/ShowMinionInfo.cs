4fa00f7572e5a2dd78de278a4404c14d9d1e6f01
Liukeyu <liukele015@gmail.com>
2025-11-08 15:56:20 +0800

    添加 ShowMinionInfo 类及其继承信息显示功能
    在 `ShowMinionInfo.cs` 文件中添加了 `ShowMinionInfo` 类，包含静态方法 `ShowInheritanceInfo`。该方法用于比较两个 `GameObject` 的 `AttributeLevels` 组件，显示工人小兵的继承信息。定义了过滤属性ID列表，并实现了属性等级处理逻辑，包括最大等级限制和面板的创建与显示。

---
+++ EternalDecay/Content/Core/ShowMinionInfo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EternalDecay.Content.Comps.KUI;
+using EternalDecay.Content.Configs;
+using Klei.AI;
+using UnityEngine;
+
+namespace EternalDecay.Content.Core
+{
+    public class ShowMinionInfo
+    {
+
+        public static void ShowInheritanceInfo(GameObject workerminion, GameObject gameObject)
+        {
+            var oldAttributes = workerminion.GetComponent<AttributeLevels>();
+            var newAttributes = gameObject.GetComponent<AttributeLevels>();
+
+            if (oldAttributes == null)
+            {
+                Debug.LogWarning("旧角色未找到 AttributeLevels 组件。");
+                return;
+            }
+
+            HashSet<string> filteredAttributes = new HashSet<string>
+            {
+                "Toggle","LifeSupport","Immunity","FarmTinker","PowerTinker"
+            }; // 要过滤掉的属性ID列表
+
+
+            List<(string attrName, int oldLevel, int newLevel)> attrList = new();
+            List<(string attrName, int oldLevel, int newLevel)> skillList = new();
+            List<(string attrName, int oldLevel, int newLevel)> traitList = new();
+
+            foreach (var oldAttrLevel in oldAttributes)
+            {
+                var attribute = oldAttrLevel.attribute.Attribute;
+                if (filteredAttributes.Contains(attribute.Id))
+                    continue;
+
+                string attrName = attribute.Name;
+                int oldLevel = oldAttrLevel.GetLevel();
+
+                var newAttrLevel = newAttributes.GetAttributeLevel(attribute.Id);
+                int newLevel = oldLevel;
+                if (newAttrLevel != null)
+                {
+                    newLevel += newAttrLevel.GetLevel();
+                    if (newLevel > TUNINGS.TIMERMANAGER.RANDOMDEBUFFTIMERMANAGER.TRANSFER.ATTRIBUTEMAXLEVEL) 
+                    {
+                        newLevel = TUNINGS.TIMERMANAGER.RANDOMDEBUFFTIMERMANAGER.TRANSFER.ATTRIBUTEMAXLEVEL;
+                    }
+                        
+                }
+
+                attrList.Add((attrName, oldLevel, newLevel));
+            }
+
+            // 创建并显示面板
+            var screenGO = new GameObject("InheritanceInfo");
+            var rectTransform = screenGO.AddComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(300f, 500f);
+
+            KScreen kscreen = screenGO.AddComponent<InheritanceInformation>();
+            GameObject panel = InheritanceInformation.Createpanel(attrList, skillList,traitList);
+
+            panel.transform.SetParent(kscreen.transform, false);
+
+            screenGO.transform.SetParent(GameObject.Find("ScreenSpaceOverlayCanvas").transform, false);
+            kscreen.Activate();
+        }
+
+    }
+}
